e2e29d844673ee375a557c0b4d6c7141f22b1b2f
woojinv <oh.woojin95@gmail.com>
2022-04-23 00:31:51 -0500

    url and link in navbar for creating ministry

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('ministries/', views.ministries_index, name='index'),
     path('ministries/<int:ministry_id>/', views.ministries_detail, name='detail'),
+    path('ministries/create/', views.MinistryCreate.as_view(), name='ministries_create'),
 ]
