39aeb387b943752048e7e58eda1b6fe928f584de
woojinv <oh.woojin95@gmail.com>
2022-04-22 22:32:56 -0500

    create Ministry Model(class)

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Ministry(models.Model):
+    name = models.CharField(max_length=100)
+    lead = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    number_of_members = models.IntegerField()
+
