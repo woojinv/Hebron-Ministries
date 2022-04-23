9a1989d420dc26fcb73b471c0cf2b461b8a84e2f
woojinv <oh.woojin95@gmail.com>
2022-04-23 12:33:51 -0500

    delete and edit ministries

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('ministries/', views.ministries_index, name='index'),
     path('ministries/<int:ministry_id>/', views.ministries_detail, name='detail'),
     path('ministries/create/', views.MinistryCreate.as_view(), name='ministries_create'),
+    path('ministries/<int:pk>/update/', views.MinistryUpdate.as_view(), name='ministries_update'),
+    path('ministries/<int:pk>/delete/', views.MinistryDelete.as_view(), name='ministries_delete'),
 ]
