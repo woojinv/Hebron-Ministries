c33ace35f94dfc22bbb3ec94d35e1a4cdb15ead4
woojinv <oh.woojin95@gmail.com>
2022-04-26 22:13:07 -0500

    change location of Member model and redirect after deleting and updating member

--- main_app/models.py
+++ main_app/models.py
@@ -2,18 +2,33 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('members_detail', kwargs={'member_id': self.id})
+
+
+
 class Ministry(models.Model):
     name = models.CharField(max_length=100)
     lead = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     number_of_members = models.IntegerField()
 
+    members = models.ManyToManyField(Member)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'ministry_id': self.id})
+
 
 
 class Event(models.Model):
@@ -31,15 +46,8 @@ class Event(models.Model):
         ordering = ['date']
 
 
-class Member(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
 
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('members_index')
+
 
     
     
