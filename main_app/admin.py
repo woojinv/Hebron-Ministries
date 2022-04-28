007a4c064718447de4f8e3b08af834670ef94374
woojinv <oh.woojin95@gmail.com>
2022-04-28 13:04:43 -0500

    able to add photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ministry, Event, Member
+from .models import Ministry, Event, Member, Photo
 # Register your models here.
 admin.site.register(Ministry)
 admin.site.register(Event)
 admin.site.register(Member)
+admin.site.register(Photo)
